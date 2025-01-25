ef576c9e781b7772cda579acedc381fa5fa8349e
asimshah <asimsshah377@gmail.com>
2025-01-25 12:25:31 +0500

    create mini-context-api

---
+++ miniContext/src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//crete the context
+const UserContext = React.createContext()
+
+export default UserContext;
+
+{/* <UserContext>
+    <login>
+        <Card>
+            <Data/> this component is attomatically accesed throw the context provider
+        </Card>
+    </login>
+</UserContext> */}
