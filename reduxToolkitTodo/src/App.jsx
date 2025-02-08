10f3759c8e01ccfc0fb038c8691a6781bbf3939d
asimshah <asimsshah377@gmail.com>
2025-02-08 19:01:28 +0500

    crete todo through redux

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <h1>learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
